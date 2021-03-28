f78dd8a8a9cccd5010205afc88b52fddfaaf40d0
Wilker Bruno <wilkerbruno16@gmail.com>
2021-03-28 20:35:50 -0300

    feat: ensure SignUp calls SaveAccessToken on success

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import {
   Footer,
   LoginHeader,
@@ -8,14 +9,16 @@ import {
 import Context from '@/presentation/contexts/form/form-context'
 import Styles from './signup-styles.scss'
 import { Validation } from '@/presentation/protocols/validation'
-import { AddAccount } from '@/domain/usecases'
+import { AddAccount, SaveAccessToken } from '@/domain/usecases'
 
 type Props = {
   validation: Validation
   addAccount: AddAccount
+  saveAccessToken: SaveAccessToken
 }
 
-const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
+const SignUp: React.FC<Props> = ({ validation, addAccount, saveAccessToken }: Props) => {
+  const history = useHistory()
   const [state, setState] = useState({
     isLoading: false,
     name: '',
@@ -56,12 +59,14 @@ const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
         return
       }
       setState(oldState => ({ ...oldState, isLoading: true }))
-      await addAccount.add({
+      const account = await addAccount.add({
         name: state.name,
         email: state.email,
         password: state.password,
         passwordConfirmation: state.passwordConfirmation
       })
+      await saveAccessToken.save(account.accessToken)
+      history.replace('/')
     } catch (error) {
       setState({ ...state, isLoading: false, mainError: error.message })
     }
